43877d3ec0fd7e5f70ede170d972d5a61221b9f8
kenosama <pc.tcanomorant@me.com>
2023-05-10 15:16:39 +0200

    🆕 feat(route.js, page.jsx): add user profile page and route to fetch user's posts The user profile page is added to display the user's posts. The route.js file is updated to fetch the user's posts from the database and return them as a response. The response is populated with the creator's details. The page.jsx file is added to fetch the user's posts and display them on the profile page. The page also includes functions to handle edit and delete actions.
    🎨 style(Profile.jsx): format code and add missing semicolons
    ✨ feat(Profile.jsx): add PunCard component to display puns
    The code has been formatted to improve readability and consistency. Semicolons have been added to the end of each statement to follow best practices. The PunCard component has been added to display puns in the profile section. The component takes in post data and handles edit and delete functionality.

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import PunCard from "./PunCard";
 
-const Profile = () => {
+const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
   return (
-    <div>Profile</div>
-  )
-}
+    <section className="w-full">
+      <h1 className="text-left head_text">
+        <span className="blue_gradient">{name} Profile</span>
+      </h1>
+      <p className="text-left desc">{desc}</p>
 
-export default Profile
+      <div className="mt-10 prompt_layout">
+        {data.map((post) => (
+          <PunCard
+            key={post._id}
+            post={post}
+            handleEdit={() => handleEdit && handleEdit(post)}
+            handleDelete={() => handleDelete && handleDelete(post)}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Profile;
