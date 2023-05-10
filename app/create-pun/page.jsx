53b679dd288b7b3db53280a90e7fa2e84e9b43a2
kenosama <pc.tcanomorant@me.com>
2023-05-10 11:43:21 +0200

    🎨 style(page.jsx): format code with prettier ✨ feat(page.jsx): add form to create puns The code has been formatted with prettier for better readability. A form has been added to create puns. The form has state for the pun and tag fields, and a submit handler that sends a POST request to the server to create a new pun. The form is wrapped in a custom Form component that handles the form's state and submission.
    🎨 style(Form.jsx): format code and add newlines at the end of the file
    ✨ feat(Form.jsx): add cancel button and improve form layout
    The code has been formatted to improve readability. A cancel button has been added to the form, which allows users to cancel the form submission and return to the home page. The form layout has been improved to make it more user-friendly.

--- app/create-pun/page.jsx
+++ app/create-pun/page.jsx
@@ -2,9 +2,45 @@
 import { useState } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
+
+import Form from "@components/Form";
 const CreatePun  = () => {
+  const [submitting, setSubmitting] = useState(false);
+  const [post , setPost ] = useState({
+    pun:'',
+    tag:'',
+  });
+
+  const createPun = async (e) =>{
+    e.preventDefault();
+    setSubmitting(true);
+
+    try {
+      const response = await fetch('/api/pun/new', {
+        method: 'POST',
+        body: JSON.stringify({
+          pun: post.pun,
+          userId: session?.user.id,
+          tag: post.tag
+        })
+      })
+      if (response.ok){
+        router.push('/');
+      }
+    } catch (error) {
+      console.log(error);
+    } finally{
+      setSubmitting(false)
+    }
+  }
   return (
-    <div>CreatePun </div>
+    <Form 
+       type="Create"
+       post={post}
+       setPost={setPost}
+       submitting={submitting}
+       handleSubmit={createPun}
+    />
   )
 }
 
--- components/Form.jsx
+++ components/Form.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link.js";
 
-const Form = () => {
+const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
   return (
-    <div>Form</div>
-  )
-}
+    <section className="flex-col w-full max-w-full flex-start">
+      <h1 className="text-left head_text">
+        <span className="blue_gradient"> {type} Post </span>
+      </h1>
+      <p className="max-w-md text-left desc">
+        {type} and share your funny Pun with the world, and let your humor be teased...
+      </p>
 
-export default Form
+      <form 
+      onSubmit={handleSubmit}
+      className="flex flex-col w-full max-w-2xl mt-10 gap-7 glassmorphism"
+      >
+        <label>
+          <span className="text-base font-semibold text-gray-700 font-satoshi">
+            Your Funny Pun
+            </span>
+
+          <textarea 
+            value={post.prompt}
+            onChange={(e)=>setPost({...post, pun: e.target.value})}
+            placeholder="Write your text here"
+            required
+            className="form_textarea"
+            ></textarea>
+        </label>
+        <label>
+          <span className="text-base font-semibold text-gray-700 font-satoshi">
+            Tag {' '} <span className="font-normal">(#funny, #darkhumor)</span>
+            </span>
+
+          <input 
+            value={post.tag}
+            onChange={(e)=>setPost({...post, tag: e.target.value})}
+            placeholder="#tag"
+            required
+            className="form_input"
+            />
+        </label>
+        <div className="gap-4 mx-3 mb-5 flex-end">
+          <Link
+          href="/"
+          className="text-sm text-gray-500" 
+          >Cancel</Link>
+          <button type="submit"
+          disabled={submitting}
+          className="px-5 py-1.5 text-sm bg-primary-orange rounded-full text-gray-50">
+            {submitting? `${type}...` : type}
+          </button>
+          
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default Form;
+ 
