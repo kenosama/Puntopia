4b6439ca3503463367d6a1f9be39c8a405845ff1
kenosama <pc.tcanomorant@me.com>
2023-05-10 10:29:30 +0200

    ✨ feat(create-pun): add CreatePun component A new CreatePun component has been added to the application. It imports the useState hook from React, useSession hook from next-auth/react, and useRouter hook from next/navigation. The component is currently a simple div element that displays the text "CreatePun".

---
+++ app/create-pun/page.jsx
@@ -0,0 +1,11 @@
+"use client";
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+const CreatePun  = () => {
+  return (
+    <div>CreatePun </div>
+  )
+}
+
+export default CreatePun 
