80ce13395ec65e23f046c15fd2033d0cb627c45a
kenosama <pc.tcanomorant@me.com>
2023-05-10 14:35:35 +0200

    🔨 refactor(Feed.jsx): refactor PunCard import and add missing semicolons ✨ feat(Feed.jsx): add search functionality to filter prompts by tag, username, and pun The PunCard import was refactored to remove the .jsx extension. Semicolons were added to the end of lines where they were missing. The search functionality was added to filter prompts by tag, username, and pun. The filterPrompts function uses a regular expression to perform a case-insensitive search. The handleSearchChange function debounces the search input and sets the searchedResults state. The handleTagClick function sets the searchText state and searchedResults state based on the clicked tag.
    🎨 style(Feed.jsx): add conditional rendering for PunCardList component
    This commit adds conditional rendering for the PunCardList component in the Feed.jsx file. The component now renders either searchedResults or allPosts based on whether searchText is truthy or falsy. This improves the user experience by allowing them to search for specific posts.
    
    🎨 style(PunCard.jsx): format code and add missing semicolon
    ✨ feat(PunCard.jsx): add copy to clipboard functionality
    The code has been formatted to improve readability and a missing semicolon has been added. The new feature added is the ability to copy the pun text to the clipboard by clicking on the copy button. The copied text is displayed as a tick mark for 3 seconds to indicate that the copy was successful.

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,38 +1,72 @@
 "use client";
-import { useState, useEffect } from "react";
-import PunCard from "./PunCard.jsx";
 
-const PunCardList = ({data, handleTagClick}) =>{
+import { useState, useEffect } from "react";
+
+import PunCard from "./PunCard";
+
+const PunCardList = ({ data, handleTagClick }) => {
   return (
     <div className="mt-16 prompt_layout">
-      {data.map((post)=> (
+      {data.map((post) => (
         <PunCard
           key={post._id}
           post={post}
           handleTagClick={handleTagClick}
-          />
+        />
       ))}
     </div>
-  )
-  }
+  );
+};
+
 const Feed = () => {
+  const [allPosts, setAllPosts] = useState([]);
+
+  // Search states
   const [searchText, setSearchText] = useState("");
-  const [posts, setPosts] = useState([]);
-  const handleSearchChange = (e) => {
-    e.prevenDefault();
+  const [searchTimeout, setSearchTimeout] = useState(null);
+  const [searchedResults, setSearchedResults] = useState([]);
+
+  const fetchPosts = async () => {
+    const response = await fetch("/api/pun");
+    const data = await response.json();
+
+    setAllPosts(data);
   };
+
   useEffect(() => {
-    const fetchPuns = async ()=>{
-      await fetch('api/pun');
-      const data = await response.json();
-
-      setPosts(data);
-
-    }
-    fetchPuns();
+    fetchPosts();
   }, []);
 
-  
+  const filterPrompts = (searchtext) => {
+    const regex = new RegExp(searchtext, "i"); // 'i' flag for case-insensitive search
+    return allPosts.filter(
+      (item) =>
+        regex.test(item.creator.username) ||
+        regex.test(item.tag) ||
+        regex.test(item.pun)
+    );
+  };
+
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+
+    // debounce method
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = filterPrompts(e.target.value);
+        setSearchedResults(searchResult);
+      }, 500)
+    );
+  };
+
+  const handleTagClick = (tagName) => {
+    setSearchText(tagName);
+
+    const searchResult = filterPrompts(tagName);
+    setSearchedResults(searchResult);
+  };
+
   return (
     <section className="feed">
       <form className="relative w-full flex-center">
@@ -45,9 +79,16 @@ const Feed = () => {
           className="search_input peer"
         />
       </form>
-      <PunCardList
-        data={posts}
-        handleTagClick={()=>{}} />
+
+      {/* All Prompts */}
+      {searchText ? (
+        <PunCardList
+          data={searchedResults}
+          handleTagClick={handleTagClick}
+        />
+      ) : (
+        <PunCardList data={allPosts} handleTagClick={handleTagClick} />
+      )}
     </section>
   );
 };
--- components/PunCard.jsx
+++ components/PunCard.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+"use client";
 
-const PunCard = () => {
+import { useState } from "react";
+import Image from "next/image.js";
+import { useSession } from "next-auth/react";
+import { usePathname, useRouter } from "next/navigation";
+
+const PunCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+  const [copied, setCopied] = useState("");
+  
+  const handleCopy = () =>{
+    setCopied(post.pun)
+    navigator.clipboard.writeText(post.pun);
+    setTimeout(()=>setCopied(""), 3000)
+  }
+
   return (
-    <div>PunCard</div>
-  )
-}
+    <div className="prompt_card">
+      <div className="flex items-start justify-between gap-5">
+        <div className="flex justify-start flex-1 gap-3 cursor-pointer item">
+          <Image
+            src={post.creator.image}
+            alt="user_image"
+            width={40}
+            height={40}
+            className="object-contain rounded-full"
+          />
 
-export default PunCard
+          <div className="flex flex-col">
+            <h3 className="font-semibold text-gray-900 font-satoshi">
+              {post.creator.username}
+            </h3>
+            <p className="text-sm text-gray-500 font-inter">
+              {post.creator.email}
+            </p>
+          </div>
+        </div>
+        <div className="copy_btn" onClick={handleCopy}>
+          <Image
+            src={
+              copied === post.pun
+                ? "/assets/icons/tick.svg"
+                : "/assets/icons/copy.svg"
+            }
+            width={12}
+            height={12}
+          />
+        </div>
+      </div>
+      <p className="my-4 text-sm text-gray-700 font-satoshi">{post.pun}</p>
+      <p
+        className="text-sm cursor-pointer font-inter blue_gradient"
+        onClick={() => handleTagClick && handleTagClick(post.tag)}
+      >
+        #{post.tag}
+      </p>
+    </div>
+  );
+};
+
+export default PunCard;
